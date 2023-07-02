c98a15c0deae3400d1f0eb5ed8ba30fa71561fcd
HmZz <85006030+hmzz804@users.noreply.github.com>
2023-07-02 10:23:32 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,15 +6,21 @@ from nonebot.adapters.onebot.v11 import Message
 import requests
 import json
 
-chazhanli = on_command("查战力", aliases={'战力查询'})
 
+def get_img_url(data:list,name:str) -> str:
+    for i in data:
+        if i['cname'] == name:
+            return i['iconUrl']
+
+
+chazhanli = on_command("查战力", aliases={'战力查询'})
 @chazhanli.handle()
-async def _handle(matcher: Matcher, total: Message = CommandArg()):
+async def handle_(matcher: Matcher, total: Message = CommandArg()):
     if total.extract_plain_text() :
         matcher.set_arg('total', total)
 
 @chazhanli.got('total', prompt="请发送英雄名称和查询战区序号，用空格分隔。(安卓qq区:1) (安卓wx区:2) (iOSqq区:3) (iOSwx区:4)" )
-async def _chazhanli(total: str = ArgPlainText('total')):
+async def chazhanli_(total: str = ArgPlainText('total')):
     total_list = total.split(" ")
     if len(total_list) != 2:
         await chazhanli.finish(message="输入格式有误，请重新输入")
@@ -24,30 +30,30 @@ async def _chazhanli(total: str = ArgPlainText('total')):
         except:
             await chazhanli.finish(message="输入格式有误，请重新输入")
     if zhanqu == '1':
-        zhanqu_num = 'qq'
+        zhanqu = 'aqq'
     elif zhanqu == '2':
-        zhanqu_num = 'wx'
+        zhanqu = 'awx'
     elif zhanqu == '3':
-        zhanqu_num = 'ios_qq'
+        zhanqu = 'iqq'
     elif zhanqu == '4':
-        zhanqu_num = 'ios_wx'
+        zhanqu = 'iwx'
     else:
         await chazhanli.finish(message='输入的战区序号有误，请重新输入')
-    url = f"https://api.iyk0.com/wzzl/?name={name}&type={zhanqu_num}"
     try:
-        dic = json.loads(requests.get(url).text)
-        url_img = dic["photo"]
+        data = (json.loads(requests.get("https://www.sapi.run/hero/getHeroList.php").text))['data']
+        url = f"https://www.sapi.run/hero/select.php?hero={name}&type="
+        dic = (json.loads(requests.get(url+zhanqu).text))['data']
     except:
         await chazhanli.finish(message="获取时出现错误，请检查输入内容后重试")
-    await chazhanli.send(message = MessageSegment.image(url_img)
-                +f'英雄昵称:{dic["name"]} \n'
-                +f'英雄描述:{dic["alias"]} \n' 
-                +f'区级地区:{dic["area"]} \n'
-                +f'区级地区最低分:{str(dic["areaPower"])} \n'
-                +f'市级地区:{dic["city"]} \n'
-                +f'市级最低分:{str(dic["cityPower"])} \n'
-                +f'省级地区:{dic["province"]} \n'
-                +f'省级地区最低分:{str(dic["provincePower"])} \n'
-                +f'设备区分:{dic["platform"]} \n'
-                +f'更新时间:{dic["updatetime"]} \n'
+    await chazhanli.finish(message = MessageSegment.image(get_img_url(data,name))
+                +f'英雄昵称: {dic["name"]} \n'
+                +f'英雄描述: {dic["alias"]} \n' 
+                +f'区级地区: {dic["area"]} \n'
+                +f'区级地区最低分: {str(dic["areaPower"])} \n'
+                +f'市级地区: {dic["city"]} \n'
+                +f'市级最低分: {str(dic["cityPower"])} \n'
+                +f'省级地区: {dic["province"]} \n'
+                +f'省级地区最低分: {str(dic["provincePower"])} \n'
+                +f'设备区分: {dic["platform"]} \n'
+                +f'更新时间: {dic["updatetime"]} \n'
     )
